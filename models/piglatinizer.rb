04e6da501ff6217c0b4deba6e3bdffc41817e1aa
Nicholas Keller <nic.keller328@gmail.com>
2020-05-07 00:55:51 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,18 @@
 class PigLatinizer
+  attr_accessor :piglatin_phrase
 
+  def piglatin_word(word)
+    if word.downcase.index(/[aeoui]/) == 0
+      word + "way"
+    else
+      first_vowel = word.index(/aeoui/)
+      first_letters = word.slice!(0..first_vowel-1)
+
+      word + first_letters + "ay"
+    end
+  end
+
+  def pig_latin_phrase(phrase)
 
 
 
